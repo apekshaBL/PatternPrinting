6e47c3e76e960f9691f055bae5c3d3236309f2c1
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-03-30 21:43:32 +0530

    Hollow Increasing Triangle pattern printing taking number as input from user

---
+++ src/PatternPrinting/HollowIncreasingTrianglePattern.java
@@ -0,0 +1,21 @@
+package PatternPrinting;
+import java.util.Scanner;
+public class HollowIncreasingTrianglePattern {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+               if(j==1 || i==n || i==j){
+                   System.out.print("*");
+               }
+               else{
+                   System.out.print(" ");
+               }
+            }
+           System.out.println();
+        }
+
+    }
+}
