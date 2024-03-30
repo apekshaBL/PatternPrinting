de97105678772dd9f53954442a1ccf45e0eb3c07
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-03-30 11:16:22 +0530

    DoubleHill Pattern printing taking number as input from user

--- src/PatternPrinting/DoubleHillPattern.java
+++ src/PatternPrinting/DoubleHillPattern.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class DoubleHillPattern {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number");
+        System.out.println("Enter number");
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n+1-i;j++){
-                System.out.print(" ");
+                System.out.print("");
             }
           for(int j=1;j<i;j++){
               System.out.print("*");
